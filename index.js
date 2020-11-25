69755051126bcb59460ca11335fabaf71885b625
Neth Maison <monumentcalifornia@gmail.com>
2020-11-25 14:00:02 -0800

    adding a UI

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const knownPass = '1234567890';
 const startTime = Date.now();
-console.log(`Started brute force at ${startTime}`);
+alert(`Started brute force at ${startTime}`);
 
 for (let i = parseInt('aaaa', 36); i <= parseInt('zzzzzz', 36); i++) {
   if (i.toString(36) === knownPass) {
     const elapsedTime = Date.now() - startTime;
 
-     return console.log(
+     alert(
       `got pass!! it is "${i.toString(36)}" took ${Math.floor(elapsedTime / 1000)} seconds`
     );
   }
-   console.log('not yet');
 }
